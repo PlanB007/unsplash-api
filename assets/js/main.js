49ab18e2ca2cbb325c3d7913c9b58cef56e21781
PlanB007 <kevinrouwkema@gmail.com>
2017-12-04 15:36:55 +0100

    Setting up things

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+//	ALL THE IMPORTS
+import Unsplash from 'unsplash-js';
+
+const unsplash = new Unsplash({
+  applicationId: "{APP_ID}",
+  secret: "{APP_SECRET}",
+  callbackUrl: "{CALLBACK_URL}"
+});
