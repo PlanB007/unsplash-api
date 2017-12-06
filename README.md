7c8b6209d718d80617145f1e3bb6fc5cba1cc48c
Kevin Rouwkema <kevinrouwkema@gmail.com>
2017-12-06 22:52:52 +0100

    added gitignore

--- README.md
+++
@@ -1,5 +0,0 @@
-# Unsplash API
->Finding out what is possible with the [Unsplash API](https://unsplash.com/developers "Unsplash dev link")
-
-## Thing to create
-- [ ] Random photo of the day
--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-//	ALL THE IMPORTS
-import Unsplash from 'unsplash-js';
-
-const unsplash = new Unsplash({
-  applicationId: "{APP_ID}",
-  secret: "{APP_SECRET}",
-  callbackUrl: "{CALLBACK_URL}"
-});
